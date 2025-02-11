839b6e402197b35ec9fc12603f7e9998e8c93621
Imre Sos <isos@cloudera.com>
2025-02-11 15:57:26 +0100

    AMCM-2982 Add our own protobufs

--- snakebite/protobuf/ClientNamenodeProtocol_pb2.py
+++ snakebite/protobuf/ClientNamenodeProtocol_pb2.py
@@ -7,6 +7,7 @@ from google.protobuf.internal import enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
+from google.protobuf import service as _service
 from google.protobuf import service_reflection
 from google.protobuf import symbol_database as _symbol_database
 # @@protoc_insertion_point(imports)
